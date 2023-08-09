ec6f957ac48a06bf859d8a96bcc9719bab90fc3b
stickerdaniel <63877413+stickerdaniel@users.noreply.github.com>
2023-08-09 15:25:54 +0200

    Retain only the lib folder

--- android/app/src/main/kotlin/com/example/lyric_lab/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lyric_lab
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lyric_lab/screens/settings/settings_screen.dart';
+
+import 'screens/settings/settings_screen.dart';
 
 void main() {
   runApp(MyApp());
